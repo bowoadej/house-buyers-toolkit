f726eafcf08b5842c7288a424f85307337f43b88
Bowo Adejuyigbe <bowo.adej@outlook.com>
2024-03-23 21:07:11 +0000

    changes to viewing checklist

---
+++ house-buyers-toolkit/src/components/PdfGenerator.tsx
@@ -0,0 +1,14 @@
+export default function PdfGeneration() {
+
+
+    return (
+        <div id="PdfGenerator">
+            <div id="button">
+                <button>Generate PDF</button>
+            </div>
+            <div id="button">
+                <button>Share</button>
+            </div>
+        </div>
+    )
+}
