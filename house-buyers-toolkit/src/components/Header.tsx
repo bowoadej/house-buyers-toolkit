23cb8fe63ac253d1b404498e02e8fa6f43a802ca
Bowo Adejuyigbe <bowo.adej@outlook.com>
2025-06-19 21:42:48 +0100

    Update Header.tsx

--- house-buyers-toolkit/src/components/Header.tsx
+++ house-buyers-toolkit/src/components/Header.tsx
@@ -5,7 +5,12 @@ export default function Header() {
     return (
         <div className='header'>
             <div className='header-container'>
+<<<<<<< Updated upstream
                 <h1>House Buyers Toolkit</h1>
+=======
+                <h1>Homely: House Buyers Toolkit</h1>
+                <p>Helping Homebuyers With Introductory Tools</p>
+>>>>>>> Stashed changes
                 <div className="subheading">
                     <Disclaimer />
 
